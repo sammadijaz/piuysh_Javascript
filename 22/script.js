81f7bc43a7b34695d8ccc0f21dcbaf06ce7ec3c7
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-10 00:26:21 +0500

    stuck 22

--- 22/script.js
+++ 22/script.js
@@ -2,12 +2,27 @@ function currentTime (){
     const time = new Date();
     const formattedTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
 
-    document.getElementById("input-time").innerText = formattedTime;
+    // const now = new Date();
+    // const timeString = now.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: "numeric", hour12: true });
+    // // console.log(timeString); // Output: e.g., "11:47 PM"
 
     
+    document.getElementById("input-time").innerText = formattedTime;
+
 } 
 
-const interval = setInterval(currentTime, 1000);
+
+const input = prompt("Timer in Seconds: ");
+
+
+function display (){
+    document.getElementById("input-time").innerText = interval + 1
+    console.log(interval +1)
+}
+
+const interval = setInterval(display, input * 1000);
+
+
 
 const btn = document.getElementById("btn");
 
