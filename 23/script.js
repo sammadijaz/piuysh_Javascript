f8be2f8cbfd2022851bbe10e476306a1bb5002c8
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-15 02:01:50 +0500

    23 done

--- 23/script.js
+++ 23/script.js
@@ -1,4 +1,39 @@
 const btn = document.getElementById("btn");
-const country = document.getElementById("country");
-const temp = document.getElementById("temp")
-const feel = document.getElementById("feel")
+const demo = document.getElementById("demo");
+const input = document.getElementById("input");
+
+
+
+function main (a){
+    // console.log(a)
+
+    function add (b){
+        console.log(a + b);
+    }
+
+    return add;
+}
+
+let addTo5 = main(5);
+
+addTo5(20); // 25
+
+let addTo100 = main(100);
+
+addTo100(5) // 105
+
+
+
+
+function fontSizer (size){
+    return () => {
+        demo.style.fontSize = `${size}px`;
+    }
+}
+
+const inputValue = (input.value);
+const customSize = fontSizer(inputValue)
+
+btn.addEventListener("click", customSize)
+
+
