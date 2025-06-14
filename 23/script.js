7c153250fcd0200dbfc7d2b2a8211bdc741930f2
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-15 01:31:49 +0500

    starting 23

---
+++ 23/script.js
@@ -0,0 +1,4 @@
+const btn = document.getElementById("btn");
+const country = document.getElementById("country");
+const temp = document.getElementById("temp")
+const feel = document.getElementById("feel")
